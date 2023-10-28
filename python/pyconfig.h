98c6cf3118c723d6e20b040d619d23cba18a4d3d
mara <mara@fail.pp.ua>
2023-10-28 23:18:35 +0300

    python: aarch64, riscv64 fixed build

---
+++ python/pyconfig.h
@@ -0,0 +1,17 @@
+/* pyconfig.h stub */
+
+#ifndef __STUB__PYCONFIG_H__
+#define __STUB__PYCONFIG_H__
+
+#if defined(__x86_64__) || \
+    defined(__sparc64__) || \
+    defined(__aarch64__) || \
+    defined(__riscv64__) || \
+    defined(__powerpc64__) || \
+    defined(__s390x__)
+#include "pyconfig-64.h"
+#else
+#include "pyconfig-32.h"
+#endif
+
+#endif
